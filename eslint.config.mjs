a61362ae0a7b9ce4a474320fdcd5c42f3c53da1f
srmartinellid-droid <srmartinelli.d@gmail.com>
2026-09-16 00:05:17 -0300

    fix: use flat ESLint config exports compatible with eslint-config-next

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  nextVitals,
+  nextTs,
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
